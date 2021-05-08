f8b9d8fbf919280e392b28c1ee05d79380d716db
theoldzheng <theoldzheng@163.com>
2021-05-08 22:03:25 +0800

    2021.5.8 SpringBoot-MVC Day12

--- src/main/java/com/atroot/admin/controller/TableController.java
+++ src/main/java/com/atroot/admin/controller/TableController.java
@@ -1,7 +1,12 @@
 package com.atroot.admin.controller;
 
+import com.atroot.admin.bean.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Description: table
@@ -12,27 +17,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TableController {
     @GetMapping("/basic_table")
-    public String basic_table(){
+    public String basic_table() {
         return "table/basic_table";
     }
 
     @GetMapping("/dynamic_table")
-    public String dynamic_table(){
+    public String dynamic_table(Model model) {
+        List<User> users = Arrays.asList(new User("zxc", "123456"), new User("zhangsan", "123456"));
+        model.addAttribute("users",users);
+
         return "table/dynamic_table";
     }
 
     @GetMapping("/editable_table")
-    public String editable_table(){
+    public String editable_table() {
         return "table/editable_table";
     }
 
     @GetMapping("/pricing_table")
-    public String pricing_table(){
+    public String pricing_table() {
         return "table/pricing_table";
     }
 
     @GetMapping("/responsive_table")
-    public String responsive_table(){
+    public String responsive_table() {
         return "table/responsive_table";
     }
 }
